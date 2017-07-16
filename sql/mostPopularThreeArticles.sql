9c05f37afc6d2eca85413a3cfc4960cde81fc9b7
Ratnadeep Abrol <ratna.abrol@googlemail.com>
2017-07-16 16:36:10 +0100

    First cut of SQL for most popular 3 articles.

--- sql/mostPopularThreeArticles.sql
+++ sql/mostPopularThreeArticles.sql
@@ -1,1 +1,7 @@
--- TODO
+-- TODO: extract subselect into view
+select title, path, num
+  from (select path, substr(path, char_length('/article/') + 1) as slug, count(*) as num from log where path like '/article/%' and status = '200 OK' group by path) as logged_articles,
+       articles
+  where logged_articles.slug = articles.slug
+  order by num desc
+  limit 3;
